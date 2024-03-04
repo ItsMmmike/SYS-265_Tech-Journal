adc71bcd91622deaaadd30ef7d22786dfe6d520e
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2024-03-04 10:51:10 -0500

    Update secure-ssh.sh

--- github-lab/linux/centos7/secure-ssh.sh
+++ github-lab/linux/centos7/secure-ssh.sh
@@ -11,10 +11,11 @@ echo "Enter Username for New SSH User: $1"
 NewUser=$1
 
 #Adds New SSH User
-useradd -m -s /bin/bash "$NewUser"
+useradd -m -d /home/"$NewUser" -s /bin/bash "$NewUser"
 mkdir -p /home/"$NewUser"/.ssh
 sudo cp /home/michael-loc/SYS-265_Tech-Journal/github-lab/linux/centos7/id_rsa.pub /home/"$NewUser"/.ssh
 sudo chmod 700 /home/"$NewUser"/.ssh
+sudo chown -R "$NewUser":"$NewUser" /home/"$NewUser"/.ssh
 
 #Removes Root SSH Login
 sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
