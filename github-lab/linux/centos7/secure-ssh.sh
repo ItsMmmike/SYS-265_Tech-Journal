2b3842b6666a2a6860f2dedb5c0b400f3dc9698a
ItsMmmike <michael.ng@mymail.champlain.edu>
2024-03-04 10:17:26 -0500

    ssh and public key stuff

---
+++ github-lab/linux/centos7/secure-ssh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#secure-ssh.sh
+#author ItsMmmike
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+
+echo "Enter Username for New SSH User: $1"
+
+NewUser=$1
+
+#Adds New SSH User
+useradd -m -s /bin/bash "$NewUser"
+mkdir -p /home/"$NewUser"/.ssh
+sudo cp /home/michael-loc/SYS-265_Tech-Journal/github-lab/linux/centos7/id_rsa.pub /home/"$NewUser"/.ssh
+sudo chmod 700 /home/"$NewUser"/.ssh
+
+#Removes Root SSH Login
+sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
